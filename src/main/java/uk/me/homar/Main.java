c90e1ecdf0f21c0abea0b5a78cc270e979ba0ccd
Heitham Omar <hhomar@gmail.com>
2023-11-01 05:38:57 +0000

    Initial commit

---
+++ src/main/java/uk/me/homar/Main.java
@@ -0,0 +1,9 @@
+import org.openapitools.client.model.UsersPostRequest;
+
+class Main {
+    public static void main(String[] args) {
+        UsersPostRequest usersPostRequest = new UsersPostRequest();
+
+        System.out.println("HELLO");
+    }
+}
